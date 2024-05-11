7b152509523d245635ce3ac5db5d3acf5b6e0010
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-05-11 16:38:37 -0400

    Completed basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/signin", element: <Signin /> },
+  { path: "/about", element: <About /> },
+  { path: "/signup", element: <SignUp /> },
+  { path: "/profile", element: <Profile /> },
+]);
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return <RouterProvider router={router} />;
 }
