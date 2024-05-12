f8e80a5690fc5c5d44c012946166be6a9b73eda2
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-05-12 14:42:15 -0400

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config(); // In order to allow for environment variables to be used.
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
