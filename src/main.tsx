a429370db866db7825e15940fcf8098d1447682d
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-05-12 14:03:54 -0400

    Created linking and header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About.tsx";
+import Home from "./pages/Home.tsx";
+import Profile from "./pages/Profile.tsx";
+import SignUp from "./pages/SignUp.tsx";
+import Signin from "./pages/Signin.tsx";
+import Header from "./components/Header.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home />, errorElement: <ErrorPage /> },
+//   { path: "/signin", element: <Signin />, errorElement: <ErrorPage /> },
+//   { path: "/about", element: <About />, errorElement: <ErrorPage /> },
+//   { path: "/signup", element: <SignUp />, errorElement: <ErrorPage /> },
+//   { path: "/profile", element: <Profile />, errorElement: <ErrorPage /> },
+//   { path: "*", element: <ErrorPage /> },
+// ]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+export default function ErrorPage() {
+  return (
+    // Write me appropriate css for this
+    <div className="flex justify-center items-center">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+    </div>
+  );
+}
