caf085fc3b225df7e109fbd9a0430926988744e2
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-06-12 15:48:14 -0400

    created test user route, working infrastructure

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/routes.user.js";
 dotenv.config(); // In order to allow for environment variables to be used.
 
 mongoose
@@ -16,3 +17,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRouter);
