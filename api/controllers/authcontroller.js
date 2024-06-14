f1432c42489e284dcecda9f2b65dc7b553c35184
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-06-14 13:19:07 -0400

    Added encrypted signin functionality

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -1,6 +1,7 @@
 import User from "../models/model.user.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +10,26 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    // We retrieve the user from the database, using errorhandler with middleware should user not be found
+    const user = await User.findOne({ username });
+    if (!user) return next(errorHandler(404, "User was not found"));
+    // compare passwords
+    const isValidPass = bcrypt.compareSync(password, user.password);
+    if (!isValidPass) return next(errorHandler(401, "Invalid credentials!"));
+
+    // extract out the password
+    const { password: pass, ...rest } = user._doc;
+    // Create a JWT for storage onto the client side
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.cookie("access-cookie", token).status(200).json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signin() {
-  return <div>Signin</div>;
+  const [formData, setFormData] = useState({});
+  const [errors, setErrors] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      // Make a request to the signup endpoint
+      const response = await fetch(`/api/auth/signin`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+
+      // If we run into errors, populate errors for user display
+      if (!response.ok) {
+        setErrors(data.message);
+        setLoading(false);
+        return;
+      }
+
+      // Otherwise, if successful, reset errors and navigate to home page
+      setLoading(false);
+      setErrors(null);
+      navigate("/");
+    } catch (error: any) {
+      setLoading(false);
+      setErrors(error.message);
+    }
+  };
+
+  return (
+    <div className="px-3 max-w-2xl mx-auto">
+      <h1 className="text-3xl text-center my-4 font-bold">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          className="border rounded-lg p-3"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="password"
+          className="border rounded-lg p-3"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        ></input>
+        <button className="bg-slate-700 rounded-lg text-white p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "Loading..." : "Sign in"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-4">
+        <p>Don't have an account?</p>
+        <Link to="/signup">
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {errors && <p className="text-red-700">{errors}</p>}
+    </div>
+  );
 }
