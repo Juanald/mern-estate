0d613349cb29af0aa6644a22c3eca034b3cbc8df
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-06-15 13:09:06 -0400

    added redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,23 +9,26 @@ import SignUp from "./pages/SignUp.tsx";
 import Signin from "./pages/Signin.tsx";
 import Header from "./components/Header.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
-import { store } from "./redux/store.tsx";
+import { persistor, store } from "./redux/store.tsx";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>
 );
--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,9 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userslice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// persistedReducer which can be added to whenever you create another reducer for another slice
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   // Setting the reducer created in the slice in the store
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
@@ -14,3 +27,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
